c9ceb8101438a003b312285b8b2fe7c3ed078a26
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-16 08:55:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
   // your code here
+	s=s.trim();
+
+	if(s==="")return "";
+
+	let spaceindex=s.indexOf(" ");
+
+	if(spaceindex===-1)return s;
+
+	return s.substring(0,spaceindex);
 }
 
-// Do not change the code below
+//Do not change the code below
+
+
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
